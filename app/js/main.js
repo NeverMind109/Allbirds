6d766723dfbe6ee72cded8f3d3d78b194a4fdc8a
NeverMind109 <vladgladkih123@gmail.com>
2022-03-22 18:19:26 +0300

    Created: Added Collections Section

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,46 @@
-$(function () {});
+$(function () {
+  $(".collections__slider").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 500,
+    prevArrow:
+      '<button class="collections__slider-btn collections__slider-prev"><img class="collections__slider-btn-img" src="images/slider-prev-btn.svg" alt=""></button>',
+    nextArrow:
+      '<button class="collections__slider-btn collections__slider-next"><img class="collections__slider-btn-img" src="images/slider-next-btn.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 993,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 763,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 577,
+        settings: {
+          arrows: false,
+          autoplay: true,
+          centerMode: true,
+          autoplaySpeed: 1000,
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 425,
+        settings: {
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 1000,
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
