45fae02570f146345e72c4436c747ef87739624d
NeverMind109 <vladgladkih123@gmail.com>
2022-03-22 21:09:43 +0300

    Created: Added Slider-Section & Subscribe Section

--- app/js/main.js
+++ app/js/main.js
@@ -92,4 +92,51 @@ $(function () {
       },
     ],
   });
+
+  $(".stories__carousel").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 500,
+    prevArrow:
+      '<button class="slider-section__slider-btn slider-section__slider-prev"><img class="slider-section__slider-btn-img" src="images/slider-prev-btn.svg" alt=""></button>',
+    nextArrow:
+      '<button class="slider-section__slider-btn slider-section__slider-next"><img class="slider-section__slider-btn-img" src="images/slider-next-btn.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 993,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 763,
+        settings: {
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 577,
+        settings: {
+          arrows: false,
+          autoplay: true,
+          centerMode: true,
+          autoplaySpeed: 1500,
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 425,
+        settings: {
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 1500,
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 });
